f8881dfb0407abc011e9cca3b4e37b0ea1148825
gabocode2907 <83044398+gabocode2907@users.noreply.github.com>
2021-04-25 12:58:21 -0500

    commint

---
+++ conditional2.js
@@ -0,0 +1,7 @@
+var name = "Bruce";
+if (name != "Bruce") {
+    console.log("¿Cuál es tu nombre?");
+} 
+else {
+    console.log("Hola, Bruce!");
+}
---
+++ conditional3.js
@@ -0,0 +1,7 @@
+var num1 = 10;
+var num2 = 15;
+if (num1 <= num2) {
+    num1 = num1 + 10;
+}
+console.log(num1);
+console.log(num2);
